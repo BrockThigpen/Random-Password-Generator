63cfb9eedff82718db2d8141da043bcd0988b235
Brock T <brockthigpen@yahoo.com>
2019-11-01 09:31:21 -0400

    added random character logic

--- script.js
+++ script.js
@@ -2,3 +2,20 @@
 const resultEl = document.getElementById('result');
 const generateEl = document.getElementById('generate');
 const clipboard = document.getElementById('clipboard');
+// random lowercase letter
+function randomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+// random uppercase letter
+function randomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+// random number
+function randomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+// random symbol
+function randomSymbols() {
+    let symbols = '!"#$%&()*+,-./:;<=>?@[]^_{|}~';
+    return symbols[Math.floor(Math.random() * symbols.length)];
+}
